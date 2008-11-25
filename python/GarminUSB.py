72d9d2109d30fa888d5f54c3eae70e4e119acc9f
Geraud Boyer <geraud@gmail.com>
2008-11-25 12:00:15 -0800

    Added garmin module

--- python/GarminUSB.py
+++ python/GarminUSB.py
@@ -1,61 +1,13 @@
 #!/usr/bin/python
-
-import sys
+import os, sys, array
 import usb
-import time
-import struct
-import array
-import math
 
-class USBError(Exception): pass
+from garmin.protocol import *
+class USBException(Exception): pass
 
 BULK_TIMEOUT=3000
 INTR_TIMEOUT=3000
 MAX_PACKET_SIZE=1024
-
-class PacketID:
-  # USB Protocol Layer
-  DATA_AVAILABLE  = 0x02
-  START_SESSION   = 0x05
-  SESSION_STARTED = 0x06
-  # Basic Link Information
-  PRODUCT_REQUEST = 0xfe
-
-class Packet:
-
-  def __init__ (self,data):
-    self.packet_id = None
-    data_as_string  = ''.join( map(chr, data) )
-    result = struct.unpack('<B3xH2xL',data_as_string[:12])
-    self.parse_data()
-
-  def id (self):
-    print 'id'
-
-  def parse_data (self):
-    print 'should parse:', self.data
-
-  def __len__ (self):
-    return len(self.data)
-
-  def __str__ (self):
-    return str(self.data)
-
-  @staticmethod
-  def encode (packet_id, payload = None, is_usb = False ):
-    payload_size = 0
-    if payload is not None:
-      payload_size = len(payload)
-    if is_usb: protocol = 0
-    else:      protocol = 20
-    message = struct.pack('<B3xH2xL', protocol, packet_id, payload_size)
-    if payload is not None and payload_size > 0:
-     message += payload
-    return message
-
-  @staticmethod
-  def dump ( data ):
-    return ''.join( map( lambda x: '\\x%02x' % ord(x), data) )
 
 
 class Garmin:
@@ -82,7 +34,7 @@ class Garmin:
           self.device = device
           break
     if self.device is None:
-      raise USBError, 'Device not found'
+      raise USBException, 'Device not found'
 
     interface = self.device.configurations[0].interfaces[0][0]
     for endpoint in interface.endpoints:
@@ -95,7 +47,7 @@ class Garmin:
       elif endpoint.type == usb.ENDPOINT_TYPE_INTERRUPT:
         if (endpoint.address & usb.ENDPOINT_DIR_MASK) == usb.ENDPOINT_IN:
           self.interrupt_in = address
-    self.handle = self.device.open)
+    self.handle = self.device.open()
     self.handle.setConfiguration(1)
     self.handle.claimInterface(0)
 
@@ -111,44 +63,44 @@ class Garmin:
 
   def read_packet (self):
     self.open()
-    data = self.handle.interruptRead( self.interrupt_in, MAX_PACKET_SIZE, INTR_TIMEOUT )
-    print 'received', data
-    return Packet( data )
+    bytes = self.handle.interruptRead( self.interrupt_in, MAX_PACKET_SIZE, INTR_TIMEOUT )
+    return Packet( array.array('B', bytes) )
 
   def write_packet (self,packet):
     self.open()
     return self.handle.bulkWrite( self.bulk_out, packet, BULK_TIMEOUT )
 
   def start_session (self):
-    packet = Packet.encode( PacketID.START_SESSION, is_usb = True )
+    packet = Packet.start_session()
     self.write_packet(packet)
     self.write_packet(packet)
     self.write_packet(packet)
 
     response = self.read_packet()
     if len(response) != 16:
-      raise USBError, 'Could not initiate session'
+      raise USBException, 'Could not initiate session'
     print 'session started'
 
   def read_a000_a0001 (self):
-    self.write_packet( Packet.encode(PacketID.PRODUCT_REQUEST) )
+    self.write_packet( Packet.encode_app(PacketID.PRODUCT_REQUEST) )
 
     while True:
       response = self.read_packet()
-      print respopnse
-      if respopnse is None:
+      packed_id = response.id()
+      if packed_id == PacketID.PRODUCT_DATA:
+        print 'product_data'
+        print response
+      elif packed_id == PacketID.EXT_PRODUCT_DATA:
+        print 'ext_product_data'
+        print response
+      elif packed_id == PacketID.PROTOCOL_ARRAY:
+        print 'protocol_array'
+        print response
         break
-      print response.id()
 
   def test (self):
     self.start_session()
     self.read_a000_a0001()
-    #@message = encode_packet(254) #struct.pack('<B3xH2x4x', 20,254)
-    #print 'Message:', dump_packet(message)
-    #print 'length:', len(message)
-    # data = self.handle.interruptRead(self.interrupt_in,64)
-    #data = self.handle.bulkRead(self.bulk_in,64)
-    #return self.handle.bulkRead( Garmin.BULK_IN, 8, 2 )
 
 def main():
   dev = Garmin()
---
+++ python/garmin/protocol.py
@@ -0,0 +1,65 @@
+import struct
+
+class ProtocolException(Exception): pass
+
+
+class PacketID:
+  # USB Protocol Layer
+  DATA_AVAILABLE  = 0x02
+  START_SESSION   = 0x05
+  SESSION_STARTED = 0x06
+  # Basic Link Information
+  PROTOCOL_ARRAY    = 0x00FD
+  PRODUCT_REQUEST   = 0x00FE
+  PRODUCT_DATA      = 0x00FF
+  EXT_PRODUCT_DATA  = 0x00F8
+
+  TRANSFER_RUNS     = 0x01C2
+
+
+class Packet(PacketID):
+
+  def __init__ (self,data):
+    self.protocol, self.packet_id, payload_length = struct.unpack('<B3xH2xL',data[:12])
+    self.payload = data[12:]
+    if len(self.payload) != payload_length:
+      raise ProtocolException, 'Incorrect payload length'
+
+  def id (self):
+    return self.packet_id
+
+  def payload (self):
+    return self.payload
+
+  def __len__ (self):
+    return len(self.payload) + 12
+
+  def __str__ (self):
+    if self.protocol == 0:
+      type_name = 'USB'
+    else:
+      type_name = 'APP'
+    payload = ' '.join( map(lambda x: '%02X' % x, self.payload) )
+    return "<Packet protocol: %s, id: %04X, length: %s, payload: %s>" % (type_name, self.packet_id, len(self.payload), payload )
+
+  @staticmethod
+  def encode_usb( packet_id , payload = None):
+    return Packet.encode(0,packet_id,payload)
+
+  @staticmethod
+  def encode_app( packet_id , payload = None):
+    return Packet.encode(20,packet_id,payload)
+
+  @staticmethod
+  def encode (protocol, packet_id, payload = None ):
+    message = struct.pack('<B3xH2xL', protocol, packet_id, len(payload or '') )
+    if payload is not None:
+     message += payload
+    return message
+
+  @staticmethod
+  def dump ( data ):
+    return ''.join( map( lambda x: '\\x%02x' % ord(x), data) )
+
+  @classmethod
+  def start_session (self): return self.encode_usb( self.START_SESSION )
